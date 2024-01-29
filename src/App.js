41e17c8c06f81f3960e7210eef72d9e2d2178894
Erdem <erdemerdogdu70@gmail.com>
2024-01-29 23:06:12 +0300

    Gereksiz yerleri kaldırma

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <iframe id="iframe" src='https://my.spline.design/untitled-95349a3cc9f2583d8899c74574ecc42d/' >annen</iframe>
-      <Home />
       <Body />
       <Team />
       <Footer />
